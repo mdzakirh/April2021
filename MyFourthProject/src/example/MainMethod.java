e16d669dd112fd660db8dc78ddbc9fccd0ba20d6
Zakir <engr.zakir.bd@gmail..com>
2021-05-19 22:02:38 -0400

    Adding notes and codes of class 7

--- MyFourthProject/src/example/MainMethod.java
+++ MyFourthProject/src/example/MainMethod.java
@@ -4,7 +4,7 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		double result = HelpingMethod.tempConvert(100);
-//		System.out.println(result);
+		System.out.println(result);
 
 		int doubleTheValueGame = HelpingMethod.doubleTheValue(20);
 		System.out.println(doubleTheValueGame);
